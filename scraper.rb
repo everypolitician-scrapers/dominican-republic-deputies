3da39d75fda29a47ee42731f13e38e026670f4e9
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-09-06 15:19:29 +0100

    Align fields for readability

--- scraper.rb
+++ scraper.rb
@@ -13,26 +13,25 @@ OpenURI::Cache.cache_path = '.cache'
 @BASE = 'http://www.parliament.gov.sl/dnn5/AboutUs/MembersofParliament.aspx'
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def scrape_list(gender, url)
   noko = noko_for(url)
   noko.css('.dip-item-zc').each do |mp|
-    data = { 
-      id: mp.css('a.dip-item-nombre/@href').text[/id=(\d+)/, 1],
-      name: mp.css('.dip-item-nombre').text.strip,
-      party: mp.css('div:contains("Bloque:")').text.sub('Bloque:','').strip,
-      area: mp.css('div:contains("Provincia:")').text.sub('Provincia:','').strip,
-      circ: mp.css('div:contains("Circ.:")').text.sub('Circ.:','').strip,
-      email: mp.css('a[href*="mailto:"]/@href').text,
-      image: mp.css('img[src*="lists_images"]/@src').text,
+    data = {
+      id:     mp.css('a.dip-item-nombre/@href').text[/id=(\d+)/, 1],
+      name:   mp.css('.dip-item-nombre').text.strip,
+      party:  mp.css('div:contains("Bloque:")').text.sub('Bloque:','').strip,
+      area:   mp.css('div:contains("Provincia:")').text.sub('Provincia:','').strip,
+      circ:   mp.css('div:contains("Circ.:")').text.sub('Circ.:','').strip,
+      email:  mp.css('a[href*="mailto:"]/@href').text,
+      image:  mp.css('img[src*="lists_images"]/@src').text,
       gender: gender,
-      term: '2010',
+      term:   '2010',
       source: url,
     }
     data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
-    # puts data
     ScraperWiki.save_sqlite([:id, :term], data)
   end
 end
